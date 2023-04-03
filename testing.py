c17d81098ed78b3c8ac393a6cd58a812f10ba93a
Majid MH <majid_mohamed@live.se>
2023-04-04 01:49:36 +0200

    python style guidlines

--- testing.py
+++ testing.py
@@ -1,14 +1,18 @@
 import unittest
 from latex_linter import linter_rules
 
+
 class test_cases(unittest.TestCase):
     def test_newlines(self):
         linter = linter_rules(["Det här är en subsection \n \subsection"])
         linter.add_blank_lines()
 
     def test_tabs(self):
-        linter = linter_rules(["\begin{center} \n Laborationsuppgifter \n \end{center}"])
+        linter = linter_rules(
+            ["\begin{center} \n Laborationsuppgifter \n \end{center}"]
+        )
         linter.tab_adding()
 
-if __name__ == '__main__':
-     unittest.main()
+
+if __name__ == "__main__":
+    unittest.main()
